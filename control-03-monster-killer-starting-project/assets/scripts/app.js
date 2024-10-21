af2338b8f6c2ce06777f992d12a723475cc63c3d
Mikaela Rhoads <mrhoads@sdccd.edu>
2024-10-21 11:11:45 -0700

    add monster attack functions

---
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -0,0 +1,41 @@
+const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
+const STRONG_ATTACK_VALUE = 17;
+let chosenMaxLife = 100;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+
+adjustHealthBars(chosenMaxLife);
+
+function attackMonster(attackMode) {
+  let maxDamage;
+  if (attackMode === 'ATTACK') {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === 'STRONG_ATTACK') {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const damange = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= damange;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert('You won!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert('You lost!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert('You have a draw!');
+  }
+}
+
+function attackHandler () {
+  attackMonster('ATTACK');
+}
+
+function strongAttackHandler() {
+  attackMonster('STRONG_ATTACK');
+}
+
+attackBtn.addEventListener('click', attackHandler);
+
+strongAttackBtn.addEventListener('click', strongAttackHandler)
+
