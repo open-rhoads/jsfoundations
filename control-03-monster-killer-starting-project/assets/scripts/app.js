c7823eda4b6b8eccb3e4e77e0dff4c56f495c895
Mikaela Rhoads <mrhoads@sdccd.edu>
2024-10-21 12:04:26 -0700

    add heal function

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,21 +1,14 @@
 const ATTACK_VALUE = 10;
 const MONSTER_ATTACK_VALUE = 14;
 const STRONG_ATTACK_VALUE = 17;
+const HEAL_VALUE = 20;
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackMonster(attackMode) {
-  let maxDamage;
-  if (attackMode === 'ATTACK') {
-    maxDamage = ATTACK_VALUE;
-  } else if (mode === 'STRONG_ATTACK') {
-    maxDamage = STRONG_ATTACK_VALUE;
-  }
-  const damange = dealMonsterDamage(maxDamage);
-  currentMonsterHealth -= damange;
+function endRound() {
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
   if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
@@ -27,6 +20,18 @@ function attackMonster(attackMode) {
   }
 }
 
+function attackMonster(attackMode) {
+  let maxDamage;
+  if (attackMode === 'ATTACK') {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === 'STRONG_ATTACK') {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const damange = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= damange;
+  endRound();
+}
+
 function attackHandler () {
   attackMonster('ATTACK');
 }
@@ -35,7 +40,20 @@ function strongAttackHandler() {
   attackMonster('STRONG_ATTACK');
 }
 
-attackBtn.addEventListener('click', attackHandler);
+function healPlayerHandler() {
+  let healValue;
+  if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+    alert('You can\'t heal to more than your max initial health.');
+    healValue = chosenMaxLife - currentPlayerHealth;
+  } else {
+    healValue = HEAL_VALUE;
+  }
+  increasePlayerHealth(healValue);
+  currentPlayerHealth += healValue;
+  endRound();
+}
 
+attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler)
+healBtn.addEventListener('click', healPlayerHandler)
 
