e961a362259f91d6de64062670c5cf9c8f555a54
Mikaela Rhoads <98623081+open-rhoads@users.noreply.github.com>
2025-03-05 17:00:12 -0800

    async/await

--- async-01-starting-setup/app.js
+++ async-01-starting-setup/app.js
@@ -33,18 +33,30 @@ const getPosition = (options) => {
   return promise;
 }
 
-function trackUserHandler() {
+// refactoring the function to leverage async/await makes it much more concise to write .then() conditions (using await instead)
+// await effectively replicates what the .then() block does behind the scenes. 
+// async creates the initial wrapping promise and then await allows you to nest more promises inside and it will automatically return the data from their callback
+async function trackUserHandler() {
   // console.log('Clicked!');
   // getCurrentPosition is a built in method of the navigator.geolocation object/property with 3 params: success callback, error callback, options object (optional)
   // let's refactor and 'promisify' it (see function above)
-  let positionData; // defining this here so we can refer to it in subsequent chained then() methods
-  getPosition() 
+  // let positionData; // defining this here so we can refer to it in subsequent chained then() methods
+  // let's add error handling - we can use a synchronous try/catch block:
+  let posData; // now that we are using try/catch, let's define these variables here so they will have global scope and be available in and out of the blocks
+  let timerData;
+  try {
+    posData = await getPosition();
+    timerData = await setTimer(2000); 
+  } catch (error) { // if the above promise fails, then this catch block will run 
+    console.log(error);
+  }
+  console.log(timerData, posData);
     // here we are using promise chaining by leveraging multiple then() methods 
     // and returning something in the first one to keep the original promise pending until all are resolved
-    .then(posData => {
-      positionData = posData;
+    // .then(posData => {
+      // positionData = posData;
       //console.log(posData);
-      return setTimer(); // returning something inside of this one will make the promise pending again until this next promise resolves
+      // return setTimer(); // returning something inside of this one will make the promise pending again until this next promise resolves
       // we are using a promise, but you can return any data and it will be wrapped inside a promise that immediately resolves 
       // and the data can be passed to the next .then() block and its callback function
       // the second callback function argument for the then() method is an error handler if the promise fails... 
@@ -52,19 +64,19 @@ function trackUserHandler() {
       // but if one of the early then() methods causes error, it will skip subsequent then() methods until it finds a catch() block or second arg for then()
       // after it finds one of those, it would continue with any subsequent then() calls (if any)... 
       // so it doesn't cancel the whole promise block unless it's at the end. It will go back into pending when it continues to execute - positioning matters
-    } //, error => {
+    // }, error => {
     //   console.log(error);
     // }
-    )  
-    .catch(
-      error => {
-        console.log(error);
-      }
-    ) 
+    // )  
+    // .catch(
+    //   error => {
+    //     console.log(error);
+    //   }
+    // ) 
     // now we can make another call to .then() before the whole promise is resolved
-    .then(data => { // this is the data from the second promise (setTimer())
-      console.log(data, positionData);
-    });
+    // .then(data => { // this is the data from the second promise (setTimer())
+    //   console.log(data, positionData);
+    // });
   // navigator.geolocation.getCurrentPosition(
   //   // first param is anonymous callback function to log the position data that is returned if successful
   //   posData => {
@@ -86,11 +98,15 @@ function trackUserHandler() {
   // what happens if we add a timer here outside the callback... this will still run after the code below 
   // because setTimeout() is passed off to the browser and has to use the message queue and event loop to get back in... 
   // even though the timer goes off after 0 milliseconds - this is the minimum time, not the guaranteed time (same for setInterval)
-  setTimer(1000).then(() => {
-    console.log('Timer done!');
-  });
+  // setTimer(1000).then(() => {
+  //   console.log('Timer done!');
+  // });
   
-  console.log('Getting Position...'); // this fires first in the console because everything else is asynchronous
+  // console.log('Getting Position...'); // this fires first in the console because everything else is asynchronous - ONLY if using raw Promises with .then() & .catch()
+  // If you use async/await, then this will only fire after ALL the promises return/resolve 
+  // because ALL the actions inside the function are wrapped inside a promise, so those actions are actually part of their own .then() block behind the scenes
+  // one potential downside...if you want something to kick off simultaneously and not all one after another
+  // there are ways around it by using a separate function for some tasks...
 }
 
 // this task is handed off to the browser so the rest of the script can continue and it's not perpetually listening for a click
@@ -103,4 +119,25 @@ button.addEventListener('click', trackUserHandler);
 // for (let i=0; i < 100000000; i++) {
 //   result += i;
 // }
+
+// Promise methods
+// what if we only want one - the faster one - to execute (i.e. wait one minute and if position not detected yet, then do something else) (could use setTimeout for this but w/e)
+// the Promise.race() method takes an array of Promise calls and it returns a Promise/data with the result of the fastest one you passed to it
+// so we can alse use then() to take action after it is returned and work with the data
+// Promise.race([getPosition(), setTimer(1000)]).then(data => {
+//   console.log(data);
+// }); // it only cares about the fastest one and other promise results will not occur and are ignored (request is still sent, just ignored)
+
+// sometimes you want something to happen only after multiple promises have occurred.
+// of course, you can chain .then() blocks, but you could also use Promise.all()
+// it returns the combined data of all the Promises
+// Promise.all([getPosition(), setTimer(1000)]).then(promiseData => {
+//   console.log(promiseData);
+// }); // if one promise fails, then the other(s) are not executed. We wait for ALL to resolve or ONE to reject
+// can handle error with catch block
+
+// if you want to wait until all resolve or failed, there is also Promise.allSettled()
+// Promise.allSettled([getPosition(), setTimer(1000)]).then(promiseData => {
+//   console.log(promiseData); // this will return the result of all, no matter what. Does not cancel any Promise actions
+// });
 
