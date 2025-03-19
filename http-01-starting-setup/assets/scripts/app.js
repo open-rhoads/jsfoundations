a814e9f3997785f3f683d3f3b7ec3ac28aca95da
Mikaela Rhoads <98623081+open-rhoads@users.noreply.github.com>
2025-03-19 16:50:37 -0700

    error handling and starting reconfigure to fetch()

--- http-01-starting-setup/assets/scripts/app.js
+++ http-01-starting-setup/assets/scripts/app.js
@@ -11,58 +11,82 @@ const postList = document.querySelector('ul'); // get the post list element
 // let's make a re-usable function for sending XML Http Requests - it accepts a method and URL
 function sendHttpRequest(method, url, data) {
   // let's create a promise for the Http Request and we can handle errors and make the HTML rendering dependent on the return of data with .then()
-  const promise = new Promise((resolve, reject) => {
+  // const promise = new Promise((resolve, reject) => {
     // Let's create an XMLHttpRequest object that will allow us to send Http requests
     // This object is built into the browser - all broswers support it
-    const httpRequest = new XMLHttpRequest();
-    // use the object to configure the request by calling the open() method
-    // just the first step to configure request, takes 2 args, the method as a string, and the URL
-    httpRequest.open(method, url); // normally you probably need credentials/a key too...
-    // If we set the responseType property to 'json' the XMLHttpRequest object will return JSON and there is no need to parse below
-    httpRequest.responseType = 'json';
-    // we can see the request in the network tab of dev tools and click it to see the data/click headers to see the info
-    // once the request completes (sends below), the load event occurs (data is loaded)
-    // what if we want to hook up the request to a DOM event - event listener
-    // XMLHttpRequests don't always support addEventListener(), so it is more common to assign a function to the .onload property
-    httpRequest.onload = function() { // this will happen asynchronously & the tasks below will continue until response is received
-      // we don't need an argument for the data; we can handle it with .response
-      // console.log(httpRequest.response); // this is returning JSON data & you must work with it as such
-      // you can also use the default JSON object in JS and its methods to parse the JSON 
-      // .stringify() to get JSON from JS values (object, array, etc) and .parse() to get JS values from JSON
-      // const listOfPosts = JSON.parse(httpRequest.response); // this will turn the JSON into a JS array
-      // pass the data to the resolve callback, if it occurs
-      if (httpRequest.status >= 200 && httpRequest.status < 300) {
-        resolve(httpRequest.response); // Resolve with the response data
-      } else {
-        reject('Request failed. Status: ' + httpRequest.status); // Reject on failure
-      }
+  //   const httpRequest = new XMLHttpRequest();
+  //   httpRequest.setRequestHeader('Content-Type', 'application/json');
+  //   // use the object to configure the request by calling the open() method
+  //   // just the first step to configure request, takes 2 args, the method as a string, and the URL
+  //   httpRequest.open(method, url); // normally you probably need credentials/a key too...
+  //   // If we set the responseType property to 'json' the XMLHttpRequest object will return JSON and there is no need to parse below
+  //   httpRequest.responseType = 'json';
+  //   // we can see the request in the network tab of dev tools and click it to see the data/click headers to see the info
+  //   // once the request completes (sends below), the load event occurs (data is loaded)
+  //   // what if we want to hook up the request to a DOM event - event listener
+  //   // XMLHttpRequests don't always support addEventListener(), so it is more common to assign a function to the .onload property
+  //   httpRequest.onload = function() { // this will happen asynchronously & the tasks below will continue until response is received
+  //     // we don't need an argument for the data; we can handle it with .response
+  //     // console.log(httpRequest.response); // this is returning JSON data & you must work with it as such
+  //     // you can also use the default JSON object in JS and its methods to parse the JSON 
+  //     // .stringify() to get JSON from JS values (object, array, etc) and .parse() to get JS values from JSON
+  //     // const listOfPosts = JSON.parse(httpRequest.response); // this will turn the JSON into a JS array
+  //     // pass the data to the resolve callback, if it occurs. Add error handling for server errors where a response is received, but it indicates server issue
+  //     // this logic handles certain error codes 
+  //     if (httpRequest.status >= 200 && httpRequest.status < 300) {
+  //       resolve(httpRequest.response); // Resolve with the response data
+  //     } else {
+  //       reject(new Error('Something went wrong!')); // Reject on failure - server issue
+  //     }
+  //   }
+  //   // handle an error - this will trigger if so. It only triggers if the error is a network failure or similar. NOT for server side issues
+  //   // if anything is returned, even an error message, then it will go to the onload event
+  //   httpRequest.onerror = function() {
+  //     reject(new Error('Failed to send request!')); // Reject on network error
+  //     console.log(response, httpRequest.status);
+  //   };
+
+  //   httpRequest.send(JSON.stringify(data)); // this actually sends the request. We can append the data passed in as a param to the response by passing it here in JSON FORM
+// });
+//   return promise; // return the promise 
+
+  // let's use the more modern fetch() API; centered around the global fetch() function in JS
+  return fetch(url, {
+    // some keys you can set here like method
+    method: method,
+    body: JSON.stringify(data),
+    headers: { // extra information for the request
+      'Content-Type': 'application/json' // this is a typical header added to JSON POST requests because it tells the server to expect JSON
     }
-    // handle an error
-    httpRequest.onerror = function() {
-      reject('Network error'); // Reject on network error
-    };
-    httpRequest.send(JSON.stringify(data)); // this actually sends the request. We can append the data passed in as a param to the response by passing it here in JSON FORM
-  });
-  return promise; // return the promise 
-} 
+  }).then(response => {
+    return response.json(); // unlike XMLHttpRequest, fetch() does not give parsed response. You have to use the response.json() method
+    // which yields a new Promise and turns the 'streamed' data into a 'snapshot'
+  }); // fetch takes a URL & an optional object parameter to configure the request. It is natively Promise based, so we don't need to build one
+  // there are also response.text() for text & response.blob() for files, but not applicable here 
+  
+}
 
 async function getPosts() {
-  const responseData = await sendHttpRequest(
-    'GET',
-    'https://jsonplaceholder.typicode.com/posts'
-  );
-  // instead of using the .response property of the httpRequest, we can tap into the responseData directly stored in the variable via async await
-  const listOfPosts = responseData; 
-  console.log(listOfPosts); // check for the data. We'll work with in inside this function because the next tasks will run while async code does & data not accessible outside
-  // let's loop through the data returned from the promise above when calling sendHttpRequest() and output HTML
-  for (const post of listOfPosts) {
-    // use for of because it's object notation
-    // let's use DOM methods to store the post template using the importNode() method
-    const postEl = document.importNode(postTemplate.content, true); // this makes a clone of the template
-    postEl.querySelector('h2').textContent = post.title.toUpperCase(); // select the heading and put the .title property value in it
-    postEl.querySelector('p').textContent = post.body; // select the paragraph and put the .body property value in it
-    postEl.querySelector('li').id = post.id; // attach the post id as the HTML id, so we can properly delete a post
-    listElement.append(postEl); // append the posts to the list element
+  try {
+    const responseData = await sendHttpRequest(
+      'GET',
+      'https://jsonplaceholder.typicode.com/posts'
+    );
+    // instead of using the .response property of the httpRequest, we can tap into the responseData directly stored in the variable via async await
+    const listOfPosts = responseData; 
+    console.log(listOfPosts); // check for the data. We'll work with in inside this function because the next tasks will run while async code does & data not accessible outside
+    // let's loop through the data returned from the promise above when calling sendHttpRequest() and output HTML
+    for (const post of listOfPosts) {
+      // use for of because it's object notation
+      // let's use DOM methods to store the post template using the importNode() method
+      const postEl = document.importNode(postTemplate.content, true); // this makes a clone of the template
+      postEl.querySelector('h2').textContent = post.title.toUpperCase(); // select the heading and put the .title property value in it
+      postEl.querySelector('p').textContent = post.body; // select the paragraph and put the .body property value in it
+      postEl.querySelector('li').id = post.id; // attach the post id as the HTML id, so we can properly delete a post
+      listElement.append(postEl); // append the posts to the list element
+    }
+  } catch (error) {
+    alert(error.message);
   }
 }
 
